ac6250d252e40a2bc29095367dea16520bf4581d
GiaBao0510 <pgiabao2002@gmail.com>
2024-05-16 08:44:51 +0700

    byGiaBao

--- app_qltv/lib/BackEndV2/app/controllers/loaiHang.controllers.js
+++ app_qltv/lib/BackEndV2/app/controllers/loaiHang.controllers.js
@@ -43,3 +43,66 @@ exports.Delete_LoaiHang = async (req, res, next) =>{
 }
 
 //3.Sửa thông tin loại hàng
+exports.update_LoaiHang = async (req, res, next) =>{
+    try{
+        //Truy vấn
+        const LOAIID = req.params.LOAIID;
+        //Thông tin cần cập nhật
+        const LOAIMA = req.body.LOAIMA,
+        LOAI_TEN = req.body.LOAI_TEN,
+        GHI_CHU = req.body.GHI_CHU,
+        SU_DUNG = req.body.SU_DUNG;
+    
+        db.query(`update loai_hang 
+                set LOAIMA = "${LOAIMA}",LOAI_TEN ="${LOAI_TEN}", GHI_CHU="${GHI_CHU}", SU_DUNG="${SU_DUNG}"    
+                where LOAIID = "${LOAIID}"`,(err, result)=>{
+            if(err){
+                console.log(`Lỗi khi cập nhật thông tin loại hàng - ${err}`);
+                return res.status(404).json({message: `Loi khi cập nhật thong tin loai hang`});
+            }else{
+
+                return res.status(200).json({message: `cập nhật thong tin loai hang thanh cong ,ID: ${LOAIID} `});
+            }
+        })
+    }catch(err){
+        return next(new ApiError(500, `Loi xuat hien khi cập nhật loai hang: ${err.message}`));
+    }
+}
+
+//4.Lấy danh sách thông tin loại hàng 
+exports.list_LoaiHang = async (req, res, next) =>{
+    try{
+
+    
+        db.query(`SELECT * FROM loai_hang`,(err, result)=>{
+            if(err){
+                console.log(`Lỗi khi lấy danh sách thông tin loại hàng - ${err}`);
+                return res.status(404).json({message: `Loi khi cập nhật thong tin loai hang`});
+            }else{
+
+                return res.status(200).json(result);
+            }
+        })
+    }catch(err){
+        return next(new ApiError(500, `Loi xuat hien khi lấy danh sách loai hang: ${err.message}`));
+    }
+}
+
+//5. Lấy thông tin loại hàng theo ID
+exports.lay_LoaiHang = async (req, res, next) =>{
+    try{
+        const LOAIID = req.params.LOAIID;
+    
+        db.query(`select * from loai_hang where LOAIID = "${LOAIID}"`,(err, result)=>{
+            if(err){
+                console.log(`Lỗi khi lấy thông tin loại hàng - ${err}`);
+                return res.status(404).json({message: `Loi khi lấy thong tin loai hang - ${LOAIID}`});
+            }else{
+
+                return res.status(200).json(result);
+            }
+        })
+    }catch(err){
+        return next(new ApiError(500, `Loi xuat hien khi xóa loai hang: ${err.message}`));
+    }
+}
--- app_qltv/lib/BackEndV2/app/routers/admin.router.js
+++ app_qltv/lib/BackEndV2/app/routers/admin.router.js
@@ -5,10 +5,15 @@ const loaihang = require('../controllers/loaiHang.controllers');
 const router = express.Router();
 
 //1. Thêm loại hàng
-router.route('/loaihang').post(loaihang.Add_LoaiHang);
+router.route('/themloaihang').post(loaihang.Add_LoaiHang);
 
 //2. Xóa và sửa loại hành
 router.route('/loaihang/:LOAIID')
+            .get(loaihang.lay_LoaiHang)
+            .put(loaihang.update_LoaiHang)
             .delete(loaihang.Delete_LoaiHang);
+
+//3.Lấy danh sách thông tin loại hàng
+router.route('/danhsachloaihang').get(loaihang.list_LoaiHang);
 
 module.exports = router;
