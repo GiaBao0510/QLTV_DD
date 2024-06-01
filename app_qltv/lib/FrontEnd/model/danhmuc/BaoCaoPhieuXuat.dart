14d7a59349fde83665956d129128f76dc1dcc867
GiaBao0510 <pgiabao2002@gmail.com>
2024-06-01 09:39:25 +0700

    Đang lam phieu xuat FE

---
+++ app_qltv/lib/FrontEnd/model/danhmuc/BaoCaoPhieuXuat.dart
@@ -0,0 +1,12 @@
+// class BaoCaoPhieuXuat_model{
+//   final String PHIEU_XUAT_MA;
+//   final String HANGHOAMA;
+//   final String HANG_HOA_TEN;
+//   final String LOAIVANG;
+//   final double CAN_TONG;
+//   final double TL_HOT;
+//   final double TL_Vang;
+//   final String NGAY_XUAT;
+//   final String ;
+//   final String ;
+// }
