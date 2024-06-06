5696912a0954d73507f8d645f244b55e408d46e8
phuongdu1407 <phuongdunguyen1407@gmail.com>
2024-06-06 15:39:30 +0700

    Update phieuRoute.js

--- app_qltv/lib/BackEnd3/app/routers/phieuRoute.js
+++ app_qltv/lib/BackEnd3/app/routers/phieuRoute.js
@@ -3,14 +3,14 @@ const phieucam = require('../controllers/baoCaoPhieu');
 const router = express.Router();
 const KiemTra = require('../services/KiemTra.services');
 
-router.route('/phieuxuat').get( /*KiemTra.CheckLogin,*/ phieucam.getPhieuXuat);
-router.route('/phieuxuattheongay').get(/*KiemTra.CheckLogin,*/  phieucam.getPhieuXuatByDate);
-router.route('/phieuxuat/:id').get( /*KiemTra.CheckLogin,*/ phieucam.getPhieuXuatById)
+router.route('/phieuxuat').get( KiemTra.CheckLogin,phieucam.getPhieuXuat);
+router.route('/phieuxuattheongay').get(KiemTra.CheckLogin,  phieucam.getPhieuXuatByDate);
+router.route('/phieuxuat/:id').get( KiemTra.CheckLogin, phieucam.getPhieuXuatById)
 router.route('/tonkho').get( KiemTra.CheckLogin, phieucam.getTonKho);
 router.route('/tonkho/:id').get( KiemTra.CheckLogin, phieucam.getTonKhoById)
 router.route('/tonkhoproduct').get( KiemTra.CheckLogin, phieucam.getTonKhoGroupProduct);
 router.route('/tonkhoproduct/:id').get( KiemTra.CheckLogin,phieucam.getTonKhoGroupProductById);
-router.route('/phieumua').get( /*KiemTra.CheckLogin,*/phieucam.getBCPhieuMuaVao);
-router.route('/phieumuatheongay').get(/*KiemTra.CheckLogin,*/phieucam.getBCPhieuMuaVaoByDate);
-router.route('/phieumua/:id').get( /*KiemTra.CheckLogin,*/phieucam.getBCPhieuMuaVaoById);
+router.route('/phieumua').get( KiemTra.CheckLogin,phieucam.getBCPhieuMuaVao);
+router.route('/phieumuatheongay').get(KiemTra.CheckLogin,phieucam.getBCPhieuMuaVaoByDate);
+router.route('/phieumua/:id').get( KiemTra.CheckLogin,phieucam.getBCPhieuMuaVaoById);
 module.exports = router;
