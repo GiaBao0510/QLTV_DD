f37f3581557d8c6c7e6eb8cea7dfaf9febef6d79
GiaBao0510 <pgiabao2002@gmail.com>
2024-06-15 16:12:20 +0700

    Thêm trang giao diện error 500

--- app_qltv/lib/BackEnd3/app/config/index_2.js
+++ app_qltv/lib/BackEnd3/app/config/index_2.js
@@ -1,15 +1,6 @@
 var mysql = require('mysql');
 // var mysql2 = require('mysql2');
 
-// var db = mysql.createConnection({
-//     host: 'baokhoagold.ddns.net',
-//     database: 'QLTV1',
-//     port: '3306',
-//     user:"user_test",
-//     password:'12345'
-
-// });
-
 // module.exports = db;
 
 var fs = require('fs')
